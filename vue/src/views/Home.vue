e79d2070044f569e7ebb263b5ababab102f1df56
xqnode <xiaqingweb@163.com>
2022-04-24 14:48:36 +0000

    update vue/src/views/Home.vue.

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -46,35 +46,6 @@
       </el-col>
     </el-row>
 
-    <el-row style="margin: 10px 0">
-
-      <el-col :span="12">
-        <el-card>
-          <div slot="header" class="clearfix">
-            <span>VIP服务</span>
-          </div>
-          <div style="text-align: left">
-            <div style="padding: 10px 0">B站：程序员青戈。 代码和资料全部在VIP群里开放</div>
-
-            <div style="padding: 10px 0">永久VIP 99元，扫下方二维码然后加我的企业微信，就可以进企业微信VIP群了。</div>
-            <div>
-              <img src="https://img-blog.csdnimg.cn/297ce70543654adfbcd34b2826e5801b.png" alt="">
-            </div>
-            <div style="line-height: 25px">
-              <h3>服务内容：</h3><br>
-
-              1. Q&A服务（问答服务）<br>
-              2. VIP代码生成器源码和使用教程<br>
-              3. B站所有的成品项目源码（持续更新）<br>
-              4. 所有笔记文档<br>
-              5. 模板之家静态模板下载服务<br>
-              6. 后续所有VIP服务内容都在（包括小程序）<br>
-              7. 必备软件下载和破解<br>
-            </div>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
   </div>
 </template>
 
